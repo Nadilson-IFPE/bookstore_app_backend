400f894ab81454ba87e2f0ae7035c755f64a70ba
Nadilson <nadilson@protonmail.com>
2023-05-08 02:45:05 -0300

    Post para Livro

--- src/main/java/com/nadilson/bookstore_app/resources/LivroResource.java
+++ src/main/java/com/nadilson/bookstore_app/resources/LivroResource.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,7 +40,14 @@ public class LivroResource {
 		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-
+	
+	@PostMapping
+	public ResponseEntity<Livro> create(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat, @RequestBody Livro obj) {
+		Livro newObj = livroService.create(id_cat, obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj) {
 		Livro newObj = livroService.update(id, obj);
@@ -53,5 +59,4 @@ public class LivroResource {
 		Livro newObj = livroService.update(id, obj);
 		return ResponseEntity.ok().body(newObj);
 	}
-	
 }
--- src/main/java/com/nadilson/bookstore_app/services/LivroService.java
+++ src/main/java/com/nadilson/bookstore_app/services/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.nadilson.bookstore_app.domain.Categoria;
 import com.nadilson.bookstore_app.domain.Livro;
 import com.nadilson.bookstore_app.repositories.LivroRepository;
 import com.nadilson.bookstore_app.services.exceptions.ObjectNotFoundException;
@@ -29,6 +30,13 @@ public class LivroService {
 		return livroRepository.findAllByCategoria(id_cat);
 	}
 	
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria categoria = categoriaService.findById(id_cat);
+		obj.setCategoria(categoria);
+		return livroRepository.save(obj);
+	}
+
 	public Livro update(Integer id, Livro obj) {
 		Livro newObj = findById(id);
 		updateData(newObj, obj);
