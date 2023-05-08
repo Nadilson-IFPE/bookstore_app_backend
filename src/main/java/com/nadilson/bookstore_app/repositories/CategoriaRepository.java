a6c47c7b112f90e2fdefd77e1a82fe5e9daa6d7d
Nadilson <nadilson@protonmail.com>
2023-05-08 02:16:18 -0300

    Put e Patch para Livro

--- src/main/java/com/nadilson/bookstore_app/repositories/CategoriaRepository.java
+++ src/main/java/com/nadilson/bookstore_app/repositories/CategoriaRepository.java
@@ -1,11 +1,9 @@
 package com.nadilson.bookstore_app.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.nadilson.bookstore_app.domain.Categoria;
 
-@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
 
 }
--- src/main/java/com/nadilson/bookstore_app/resources/LivroResource.java
+++ src/main/java/com/nadilson/bookstore_app/resources/LivroResource.java
@@ -1,15 +1,22 @@
 package com.nadilson.bookstore_app.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nadilson.bookstore_app.domain.Livro;
 import com.nadilson.bookstore_app.dtos.LivroDTO;
@@ -34,7 +41,17 @@ public class LivroResource {
 		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
-	
 
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj) {
+		Livro newObj = livroService.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
+	}
+	
+	@PatchMapping(value = "/{id}")
+	public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj) {
+		Livro newObj = livroService.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
+	}
+	
 }
